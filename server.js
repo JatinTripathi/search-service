9b70e6e8bf403d93c3360c2027ac5d89d1eaaf74
Jatin Tripathi <pratap.jatintripathi@gmail.com>
2016-03-16 08:51:11 +0000

    init

--- server.js
+++ server.js
@@ -3,38 +3,72 @@ var logger=require('logger');
 var morgan=require('morgan');
 var path=require('path');
 var elasticSearch=require('elasticsearch');
+var bodyParser=require('body-parser');
+
 
 
 //==============Express Config==================//
 var app=express();
+app.use(bodyParser.urlencoded({extended:false}));
+//==========Search Config
 var elasticClient=new elasticSearch.Client({
     host:'search',
     log:'./log/searchlogs'});
-
+//===============View Config
 app.use('views',path.join(__dirname,'views'));
 app.use('view engine','jade');
-
+//===============Logger Config
 app.use(morgan(':method :url :status :response-time ms - :res[content-length]',{'stream':logger.stream}));
 
 
-//====================Index Routing and Querying============//
-app.get('/search',function(req,res){
-    elasticClient.search({
-        index:'publishedDoc',
-        query:{
-            match:{
-                body:req.query.search
+
+//=========================Mapping=========================//
+elasticClient.indices.putMapping({
+    index:'publishedDoc',
+    type:'document',
+    body:{
+        properties:{
+            title:{type:String},
+            shortScript:{type:String},
+            suggest:{
+                type:'completion',
+                analyzer:'simple',
+                search_analyzer: "simple",
+                payloads: true
+                
             }
         }
-    }).then(function(resp){
-        
-    }),function(err){
+    }
+});
+
+
+//====================Index Routing and Querying============//
+//====================Mapping Documents
+app.post('/map',function(req,res){
+    
+});
+
+//===================Query SearchedTearms
+app.get('/search*',function(req,res){
+    elasticClient.search({
+        index:'publishedDoc',
+        body:{
+            query:{
+                match:{
+                    body:req.query.terms
+                }
+            }
+        },
+    }),function(err,results){
         if(err) throw err;
+        res.render('result',{result:results.hits.hits});
     };
 });
 
 
+
 //==================Port Config==================//
 var port=process.env.port||8080;
-app.listen(port);
-logger.info('Server listening at port'+port+'!');
+app.listen(port,function(){
+    logger.info('Server listening at port'+port+'!');
+});
