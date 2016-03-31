df9e33b5286614c844948dbffec32669df4b7798
Jatin Tripathi <pratap.jatintripathi@gmail.com>
2016-03-31 12:27:50 +0000

    new promises

--- server.js
+++ server.js
@@ -1,133 +1,110 @@
-var express=require('express');
-//var logger=require('./log/logger.js');
-var morgan=require('morgan');
-var path=require('path');
-var elasticSearch=require('elasticsearch');
-var bodyParser=require('body-parser');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-var indexName='publisheddoc';
-var typeName='documents';
+var app = express();
 
-//==============Express Config==================//
-var app=express();
-app.use(bodyParser.urlencoded({extended:false}));
-//==========Search Config
-var elasticClient=new elasticSearch.Client({
-    host:'search:9200'});
-//===============View Config
-app.set('views',path.join(__dirname,'views'));
-app.set('view engine','jade');
-//===============Logger Config
-//app.use(morgan(':method :url :status :response-time ms - :res[content-length]',{'stream':logger.stream}));
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-
-//====================Elasticsearch Connection Test=======//
-elasticClient.ping({
-  // ping usually has a 3000ms timeout
-  requestTimeout: Infinity,
-
-  // undocumented params are appended to the query string
-  hello: "elasticsearch!"
-}, function (error) {
-  if (error) {
-    console.trace('elasticsearch cluster is down!');
-  } else {
-    console.log('All is well');
-  }
-});
-
-//=========================Initiate And Map=========================//
-elasticClient.indices.create({
-    index:indexName
-}).then(function(result){
-    console.log(result)
-});
-
-elasticClient.indices.putMapping({
-    index:indexName,
-    type:typeName,
-    body:{
-        properties:{
-            head:{type:"string"},
-            shortscript:{type:"string"},
-            suggest: {
-                    type: "completion",
-                    analyzer: "simple",
-                    search_analyzer: "simple",
-                    payloads: true
-                }
-        }
-    }
-}).then(function(result){
-    console.log(result)
-});
-
-//====================Index Routing Querying and Suggestion============//
-//====================Indexing Documents
-app.post('/index',function(req,res){
-    elasticClient.index({
-        index:indexName,
-        type:typeName,
-        body:{
-            head:req.body.head,
-            shortscript:req.body.shortScript,
-            suggest: {
-                input: req.body.head.split(" "),
-                output: req.body.head,
-                payload: req.metadata || {}
-            }
-        }
-    }).then(function(result){
-        res.json(result);
-    });
-});
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 
-//===================Query Suggestion
-app.get('/suggest/:term',function(req,res,next){
-    elasticClient.suggest({
-        index:indexName,
-        type:typeName,
-        body:{
-            docsuggest:{
-                text:req.params.term,
-                completion:{
-                    field:'suggest',
-                    fuzzy:true
-                }
-            }
-        }
-    }).then(function(result){
-        res.json(result);
-        
-    });
-});
 
-//===================Query SearchedTearms
-app.get('/search/:term',function(req,res){
-    elasticClient.search({
-        index:indexName,
-        type:typeName,
-        body:{
-            query:{
-                match:{
-                    body:req.params.term
-                }
-            }
-        },
-    }).then(function(result){
-        res.json(result);
-        
-    });
-});
-
+var elastic = require('./elasticsearch');
+// Get Editor for input
 app.get('/editor',function(req,res){
-    res.render('editor');
+  res.render('editor');
+});
+
+/* GET suggestions */
+app.get('/suggest/:input', function (req, res, next) {
+  elastic.getSuggestions(req.params.input).then(function (result) { res.json(result) });
+});
+
+/* POST document to be indexed */
+app.post('/index', function (req, res, next) {
+  elastic.addDocument(req.body).then(function (result) { res.json(result) });
+});
+
+/* GET home page. */
+app.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
 
-//==================Port Config==================//
-var port=process.env.port||8080;
-app.listen(port,function(){
-    console.log('Server listening at port'+port+'!');
+
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+var elastic = require('./elasticsearch');
+elastic.indexExists().then(function (exists) {
+  if (exists) {
+    return elastic.deleteIndex();
+  }
+}).then(function () {
+  return elastic.initIndex().then(elastic.initMapping).then(function () {
+    //Add a few book titles for the autocomplete
+    //elasticsearch offers a bulk functionality as well, but this is for a different time
+    var promises = [
+      'Thing Explainer',
+      'The Internet Is a Playground',
+      'The Pragmatic Programmer',
+      'The Hitchhikers Guide to the Galaxy',
+      'Trial of the Clone',
+      'All Quiet on the Western Front',
+      'The Animal Farm',
+      'The Circle'
+    ].map(function (bookTitle) {
+      return elastic.addDocument({
+        title: bookTitle,
+        content: bookTitle + " content!",
+        metadata: {
+          titleLength: bookTitle.length
+        }
+      });
+    });
+    return Promise.all(promises);
+  });
+});
+
+module.exports = app;
